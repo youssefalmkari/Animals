363763b9545262cf9f7c5008938a36cfe6589bc2
Youssef Almkari <youssefalmkari@gmail.com>
2021-02-07 13:42:31 -0600

    Use Palette to set screen background based on displayed image color

--- app/src/main/java/com/example/animals/view/DetailFragment.kt
+++ app/src/main/java/com/example/animals/view/DetailFragment.kt
@@ -1,10 +1,17 @@
 package com.example.animals.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.GenericLifecycleObserver
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.animals.R
 import com.example.animals.model.Animal
 import com.example.animals.util.getProgressDrawable
@@ -14,6 +21,7 @@ import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.item_animal.*
 import kotlinx.android.synthetic.main.item_animal.animalImage
 import kotlinx.android.synthetic.main.item_animal.animalName
+import kotlinx.android.synthetic.main.fragment_detail.animalLayout as animalLayout1
 
 class DetailFragment : Fragment() {
 
@@ -42,5 +50,27 @@ class DetailFragment : Fragment() {
         animalLocation.text = animal?.location
         animalLifespan.text = animal?.lifeSpan
         animalDiet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+    }
+
+    fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate() {palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+                            animalLayout1.setBackgroundColor(intColor)
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+            })
     }
 }

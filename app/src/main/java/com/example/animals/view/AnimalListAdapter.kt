50cdf13881bc1710ca562f67f25d139df4a2dc18
Youssef Almkari <youssefalmkari@gmail.com>
2021-02-07 13:14:23 -0600

    Add fragment navigation between list and detail screens
    Using Navigation lib, it is easy to add arguments to fragments and use actions to carry over that information.

--- app/src/main/java/com/example/animals/view/AnimalListAdapter.kt
+++ app/src/main/java/com/example/animals/view/AnimalListAdapter.kt
@@ -3,6 +3,7 @@ package com.example.animals.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animals.R
 import com.example.animals.model.Animal
@@ -34,6 +35,10 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
             animalList[position].imageUrl,
             getProgressDrawable(holder.view.context)
         )
+        holder.view.animalLayout.setOnClickListener {
+            val action = ListFragmentDirections.actionDetail(animalList[position])
+            Navigation.findNavController(holder.view).navigate(action)
+        }
     }
 
     override fun getItemCount() = animalList.size
--- app/src/main/java/com/example/animals/view/DetailFragment.kt
+++ app/src/main/java/com/example/animals/view/DetailFragment.kt
@@ -6,8 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.animals.R
+import com.example.animals.model.Animal
+import com.example.animals.util.getProgressDrawable
+import com.example.animals.util.loadImage
+import com.example.animals.view.DetailFragmentArgs.fromBundle
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.item_animal.*
+import kotlinx.android.synthetic.main.item_animal.animalImage
+import kotlinx.android.synthetic.main.item_animal.animalName
 
 class DetailFragment : Fragment() {
+
+    var animal: Animal? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +25,22 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            animal = DetailFragmentArgs.fromBundle(it).animal
+        }
+
+        context?.let {
+            animalImage.loadImage(animal?.imageUrl, getProgressDrawable(it))
+        }
+
+        animalName.text = animal?.name
+        animalLocation.text = animal?.location
+        animalLifespan.text = animal?.lifeSpan
+        animalDiet.text = animal?.diet
     }
 }
